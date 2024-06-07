f7ee9184b1b19e244200a04737ab460b82305b80
JuanCruzLendoiro <168621053+JuanCruzLendoiro@users.noreply.github.com>
2024-06-07 20:35:05 -0300

    Update SortTask.java

--- QuickSort/src/modelo/SortTask.java
+++ QuickSort/src/modelo/SortTask.java
@@ -19,15 +19,15 @@ public class SortTask extends RecursiveAction {
 	// Metodo recursivo con concurrencia
 	@Override
 	protected void compute() {
-		if (izquierda >= derecha) {												// Condicion de corte de recursividad
-			return;																// Si se cumple la condicion, quiere decir que izquierda es igual o mayor a derecha y
-		}																		// no tiene sentido continuar ordenando
+		if (izquierda >= derecha) {						// Condicion de corte de recursividad
+			return;								// Si se cumple la condicion, quiere decir que izquierda es igual o mayor a derecha y
+		}									// no tiene sentido continuar ordenando
 
-		int pivotPos = QuickSort.partition(vector, izquierda, derecha);			// Se obtiene la posicion del pivot
-		SortTask firstHalfTask = new SortTask(vector, izquierda, pivotPos);		// Primera tarea: QuickSort del sub-arreglo a la izquierda del pivot
+		int pivotPos = QuickSort.partition(vector, izquierda, derecha);		// Se obtiene la posicion del pivot
+		SortTask firstHalfTask = new SortTask(vector, izquierda, pivotPos);	// Primera tarea: QuickSort del sub-arreglo a la izquierda del pivot
 		SortTask secondHalfTask = new SortTask(vector, pivotPos + 1, derecha);	// Segunda tarea: QuickSort del sub-arreglo a la derecha del pivot
 
-		invokeAll(firstHalfTask, secondHalfTask);								// Tareas que se van a ejecutar en paralelo
+		invokeAll(firstHalfTask, secondHalfTask);				// Tareas que se van a ejecutar en paralelo
 	}
 
 }
